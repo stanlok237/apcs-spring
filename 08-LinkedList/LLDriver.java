f7f529ba19162d904ab9131d05b76e5ea1ba924c
Stanley Lok <stanlok237@gmail.com>
2014-03-19 21:47:06 -0400

    Finish Homework 8

--- 08-LinkedList/LLDriver.java
+++ 08-LinkedList/LLDriver.java
@@ -1,20 +1,54 @@
 public class LLDriver {
     public static void main(String[] args){
 	MyLinkedList L = new MyLinkedList();
-	System.out.println(L);
+	//System.out.println(L);
 	L.add("Sully");
-	System.out.println(L);
+	//System.out.println(L);
 	L.add("Mike");
-	System.out.println(L);
+	//System.out.println(L);
 	L.add("Randall");
-	System.out.println(L);
+	//System.out.println(L);
 	L.add("Boo");
+	//System.out.println(L);
+
+	L.add(0,"Steve");
+	
 	System.out.println(L);
+	System.out.println();
 
-	L.add(2,"Steve");
-	System.out.println(L);
+	
+	System.out.println(L.get(0));
+	System.out.println(L.get(1));
+	System.out.println(L.get(2));
+	System.out.println(L.get(3));
+	System.out.println(L.get(4));
+	
+	System.out.println();
+	System.out.println(L.set(0,"God"));
+	System.out.println();
 
+	System.out.println(L.get(0));
+	System.out.println(L.get(1));
+	System.out.println(L.get(2));
+	System.out.println(L.get(3));
+	System.out.println(L.get(4));
 
+	System.out.println();
+	System.out.println(L.remove(0));
+	System.out.println();
 
+	System.out.println(L.get(0));
+	System.out.println(L.get(1));
+	System.out.println(L.get(2));
+	System.out.println(L.get(3));
+	//System.out.println(L.get(4));
+	
+	System.out.println();
+	System.out.println(L.find("Steve"));
+	//System.out.println(L);
+	System.out.println();
+
+	System.out.println(L.length());
+	
     }
 }
--- 08-LinkedList/MyLinkedList.java
+++ 08-LinkedList/MyLinkedList.java
@@ -1,6 +1,11 @@
 public class MyLinkedList {
     private Node head;
 
+    /*My Add method works correctly when I check it by calling get to
+      each index and printing it out.  However, the toString() seems to
+      throw it off when I try to print the LinkedList.  I'm not
+      sure what is wrong with it
+    */
 
     public MyLinkedList() {
 	head = null;
@@ -12,9 +17,8 @@ public class MyLinkedList {
 	head=tmp; // you must do this second
 
     }
-    
+    //Doesn't work properly
     public String toString() {
-	int i = 0;
 	Node tmp = head;
 	String s = "";
 	while(tmp != null){
@@ -34,31 +38,78 @@ public class MyLinkedList {
 	    tmp2 = tmp2.getNext();
 	    i++;
 	}
-	System.out.println(tmp2);
+	//	System.out.println(tmp2);
 	tmp.setNext(tmp2.getNext());
 	tmp2.setNext(tmp);
 	head = tmp2;
     }
-    /*
-    public String get(int i){
-
+    
+    public String get(int index){
+	Node tmp = head;
+	int i = 0;
+	while(i < index){
+	    tmp = tmp.getNext();
+	    i++;
+	}
+	return tmp.toString();
     }
-
-    public String set(int i, String s){
+    
+    
+    public String set(int index, String s){
+	Node tmp = head;
+	//	Node tmp2 = new Node(s);
+	int i = 0;
+	while(i < index-1){
+	    tmp = tmp.getNext();
+	    i++;
+	}
+	//System.out.println(tmp2);
+	//tmp2.setNext(tmp.getNext().getNext());
+	String ans = tmp.toString();
+	tmp.setData(s);
+	return ans;
+    }
+    
+    public String remove(int index){
+	if(index == 0){
+	    String ans = head.toString();
+	    head = head.getNext();
+	    return ans;
+	}
+	Node tmp = head;
+	int i = 0;
+	while(i<index - 1){
+	    tmp = tmp.getNext();
+	    i++;
+	}
+	String ans = tmp.toString();
+	tmp.setNext(tmp.getNext().getNext());
+	return ans;
 	
     }
-
-    public String remove(int i){
-
-    }
-
+    
     public int find(String s){
-
+	Node tmp = head;
+	int i = 0;
+	while(tmp != null){
+	    if(tmp.toString().equals(s)){
+		return i;
+	    }
+	    tmp = tmp.getNext();
+	    i++;
+	}
+	return -1;
     }
-
+    
     public int length(){
-
+	int length = 0;
+	Node tmp = head;
+	while(tmp!= null){
+	    tmp = tmp.getNext();
+	    length++;
+	}
+	return length;
     }
 
-*/
+
 }
