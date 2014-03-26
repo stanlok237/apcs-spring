3883c7e4c93c21cc8b5eb98233dcaba35231ac27
Stanley Lok <stanlok237@gmail.com>
2014-03-25 22:51:29 -0400

    Iterator Error missing identifier

--- 09-LinkedList3/MyLLIterator.java
+++ 09-LinkedList3/MyLLIterator.java
@@ -5,4 +5,24 @@ public class MyLLIterator<E> implements Iterator <E>{
 
     private Node<E> currentNode;
 
-    public MyLLIterator<E>(Node<E> n){
+    public MyLLIterator<E>(Node n){
+	currentNode = n;
+    }
+
+    public boolean hasNext(){
+	if(currentNode.getNext() != null ){
+	    return true;
+	}
+	return false;
+    }
+
+    public E next(){
+	E ans = currentNode.getData();
+	currentNode = currentNode.getNext();
+	return ans;
+    }
+
+    public void remove(){
+    }
+
+}
