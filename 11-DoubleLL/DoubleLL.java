1c2e5402ee172de3c170c2080a89a4c5a2797c6e
Stanley Lok <stanlok237@gmail.com>
2014-03-27 21:26:30 -0400

    Made DoubleLL circular

--- 11-DoubleLL/DoubleLL.java
+++ 11-DoubleLL/DoubleLL.java
@@ -47,21 +47,53 @@ public class DoubleLL<E> {
 	    current = n;
 	}
 	else{
-	    //This fixes the insert problem of deleting elements
-	    //It inserts the element you want directly after the current node
-	    n.prev = current;
-	    n.next = current.next;
-	    current.next = n;
-	    current = n;
-	    /*
 	    n.next = current;
 	    n.prev = current.prev;
+	    if(current.prev != null){
+		//Prevents deletion of elements
+		current.prev.next = n;
+	    }
 	    current.prev = n;
 	    current = n;
-	    */
 	}
     }
-
+    public boolean find(E d){
+	Node<E> tmp = current;
+	if(tmp == null){
+	    return false;
+	}
+	while(tmp.getPrev() != null){
+	    tmp = tmp.getPrev();
+	}
+	while(tmp != null){
+	    if(tmp.toString().equals(d.toString())){
+		return true;
+	    }
+	    tmp = tmp.getNext();
+	}
+	return false;
+    }
+    
+    public E delete(E d){
+	Node<E> tmp = current;
+	if(tmp == null){
+	    return null;
+	}
+	while(tmp.getPrev() != null){
+	    tmp = tmp.getPrev();
+	}
+	while(tmp != null){
+	    if(tmp.toString().equals(d.toString())){
+		E ans = tmp.getData();
+		Node<E> tmp2 = tmp.getNext();
+		tmp.getPrev().setNext(tmp2);
+		return ans;
+	    }
+	    tmp = tmp.getNext();
+	}
+	return null;
+    }
+    
     public E getCurrent(){
 	return current.getData();
     }
@@ -80,19 +112,48 @@ public class DoubleLL<E> {
     }
 
     public String toString(){
+	
+	//Circular toString()
+	///*
+	Node<E> tmp = current;
+	if(tmp == null){
+	    return "";
+	}
+	String s = tmp.toString() + " ";
+	//System.out.println(s);
+	String tmps = tmp.toString();
+	while(tmp.getNext() != null){
+	    //System.out.println(s + "\n");
+	    s += tmp.getNext().getData() + " ";
+	    tmp = tmp.getNext();
+	}
+	while (tmp.getPrev() != null){
+	    //System.out.println();
+	    //System.out.println(tmp);
+	    tmp = tmp.getPrev();
+	    //System.out.println(tmp);
+	    //System.out.println();
+	}
+	while(!tmp.toString().equals(tmps)){
+	    //System.out.println("test");
+	    s += tmp.getData() + " ";
+	    tmp = tmp.getNext();
+	}
+	//*/
+	/* Old toString()
+	Node<E> tmp = current;
 	if(current == null){
 	    return "";
 	}
-	while (current.getPrev() != null){
-	    current = current.getPrev();
+	while (tmp.getPrev() != null){
+	    tmp = tmp.getPrev();
 	}
-
-	Node<E> tmp = current;
-	String s = "";
-	while (tmp!= null){
-	    s = s + tmp.getData() + " ";
+	while (tmp != null){
+	    s = tmp.getData() + " " + s;
 	    tmp = tmp.getNext();
 	}
+	s+= "\n";
+	*/
 	return s;
     }
 
@@ -113,6 +174,9 @@ public class DoubleLL<E> {
 	L.insert("inserted");
 	System.out.println(L);
 	//	*/
-
+	System.out.println(L.find("world"));
+	
+	System.out.println(L.delete("world"));
+	System.out.println(L);
     }
 }
